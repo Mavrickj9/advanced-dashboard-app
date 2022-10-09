6925d4fbe2faf04eb0e8827e2d9babe5a95db3fa
Tinotenda Mupasiri <mavrickj9@gmail.com>
2022-10-09 16:04:57 +0200

    fixed import cycle and product8 errors

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,11 +1,16 @@
 export { default as Button } from './Button';
 export { default as ThemeSettings } from './ThemeSettings';
 export { default as Sidebar } from './Sidebar';
+// eslint-disable-next-line import/no-cycle
 export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
+// eslint-disable-next-line import/no-cycle
 export { default as Cart } from './Cart';
+// eslint-disable-next-line import/no-cycle
 export { default as Chat } from './Chat';
+// eslint-disable-next-line import/no-cycle
 export { default as Notification } from './Notification';
+// eslint-disable-next-line import/no-cycle
 export { default as UserProfile } from './UserProfile';
 export { default as SparkLine } from './Charts/SparkLine';
 export { default as LineChart } from './Charts/LineChart';
